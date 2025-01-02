e83ba3ae0506c5e8777767fc92741856c1548213
Zangetsu <20190407745@asose.in>
2025-01-02 11:59:55 +0000

    globe realted bug's

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero />
-        {/* <Grid /> */}
+        <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
